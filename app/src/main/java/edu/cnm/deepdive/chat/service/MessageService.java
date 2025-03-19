04fda6449968bcf50ef6dc46c20a5a870ac49c2a
Aaron Halbig <aaronhalbig@gmail.com>
2025-03-19 10:57:10 -0600

    Working on message viewmodel.

--- app/src/main/java/edu/cnm/deepdive/chat/service/MessageService.java
+++ app/src/main/java/edu/cnm/deepdive/chat/service/MessageService.java
@@ -14,36 +14,46 @@ import javax.inject.Singleton;
 @Singleton
 public class MessageService {
 
+  private static final String BEARER_TOKEN_FORMAT = "Bearer %s";
+
   private final ChatServiceProxy proxy;
   private final ChatServiceLongPollingProxy longPollingProxy;
+  private final GoogleSignInService signInService;
   private final Scheduler scheduler;
 
   @Inject
-  public MessageService(ChatServiceProxy proxy, ChatServiceLongPollingProxy longPollingProxy) {
+  public MessageService(ChatServiceProxy proxy, ChatServiceLongPollingProxy longPollingProxy,
+      GoogleSignInService signInService) {
     this.proxy = proxy;
     this.longPollingProxy = longPollingProxy;
+    this.signInService = signInService;
     scheduler = Schedulers.io();
   }
 
   Single<List<Message>> getMessages(UUID channelKey, Instant since) {
-    // TODO: 3/19/25 Refresh bearer token and pass downstream
-    return longPollingProxy
-        .getSince(channelKey, since.toEpochMilli())
-    .subscribeOn(scheduler);
+    return signInService
+        .refreshBearerToken()
+        .observeOn(scheduler)
+        .map((token) -> String.format(BEARER_TOKEN_FORMAT, token))
+        .flatMap((bearerToken) -> longPollingProxy
+            .getSince(channelKey, since.toEpochMilli(), bearerToken));
   }
 
   Single<List<Message>> sendMessage(UUID channelKey, Message message, Instant since) {
-    // TODO: 3/19/25 Refresh bearer token and pass downstream
-    return proxy
-        .postMessage(message, channelKey, since.toEpochMilli())
-        .subscribeOn(scheduler);
+    return signInService
+        .refreshBearerToken()
+        .observeOn(scheduler)
+        .map((token) -> String.format(BEARER_TOKEN_FORMAT, token))
+        .flatMap((bearerToken) -> proxy
+            .postMessage(message, channelKey, since.toEpochMilli(), bearerToken));
   }
 
   Single<List<Channel>> getChannels(boolean active) {
-    // TODO: 3/19/25 Refresh bearer token and pass downstream
-    return proxy
-        .getChannels(active)
-        .subscribeOn(scheduler);
+    return signInService
+        .refreshBearerToken()
+        .observeOn(scheduler)
+        .map((token) -> String.format(BEARER_TOKEN_FORMAT, token))
+        .flatMap((bearerToken) -> proxy.getChannels(active, bearerToken));
   }
 
 }
